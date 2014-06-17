614e0004c0ff8b6ef6243aac5f69be61bd65f8ed
Gianluca Skriczka <josephpmay@yahoo.co.uk>
2014-06-17 14:38:45 +0100

    Update currency-converter.py

--- currency-converter.py
+++ currency-converter.py
@@ -1,32 +1,67 @@
-allowables = ["pounds", "dollars", "euro", "yen"]
-rates = [1,2,3,4]
-pounds = 'pounds'
-dollars = 'dollars'
-yen = 'yen'
-euro = 'euro'
-print("Welcome to the currency converter")
+currencies= {
+    "Pound Sterling": 1, 
+    "Euro": 1.2, 
+    "US Dollar": 1.6, 
+    "Japanese Yen": 200
+    }
 
-var1 = None
-while var1 not in range(len(allowables)):
-    print('Please type the currency code you wish to convert from')
-    for index, currency in enumerate(allowables):
-        print ('enter {0} for {1}'.format(index, currency))
-    var1 = input("Please type what currency you wish to convert from ")
-var1 = int(var1)
+short_hand = {
+    "GBP": "Pound Sterling",
+    "EUR": "Euro",
+    "USD": "US Dollar",
+    "JPY": "Japanese Yen"
+    }
+    
+c_type1 = raw_input("What Currency are you converting from? GBP/EUR/USD/JPY: ")
 
-var2 = None
-while var2 not in range(len(allowables)):
-    print('Please type the currency code you wish to convert to')
-    for index, currency in enumerate(allowables):
-        print ('enter {0} for {1}'.format(index, currency))
-    var2 = input("Please type the currency that you wish to convert to ")
-var2 = int(var2)
+if c_type1 == "GBP" or "EUR" or "USD" or "JPY":
+    "You entered %s" %(c_type1)
+else:
+    print c_type1, "is not a valid input"
+    c_type1 = raw_input("Please enter a valid currency to convert from- GBP/EUR/USD/JPY: ")
+    
+c_type2 = raw_input("What Currency are you converting to? GBP/EUR/USD/JPY: ")
 
-var3 = float(input("Please type the amount of currency you wish to convert "))
+if c_type2 != "GBP" or "EUR" or "USD" or "JPY":
+    "You entered %s" %(c_type1)
+else:
+    print c_type2, "is not a valid input"
+    c_type2 = raw_input("Please enter a valid currency to convert to- GBP/EUR/USD/JPY: ")
 
-ammount = var3/rates[var1] *rates[var2]
-print(' your converted ammount is {0} {1}'.format(ammount,allowables[var2]))
+def rate(c_type1, c_type2):
+    if c_type1 == "GBP":
+        print "exchange rate is", currencies["Pound Sterling"]
+    if c_type1 == "EUR":
+        print "exchange rate is", currencies["Euro"]
+    if c_type1 == "USD":
+        print "exchange rate is", currencies["US Dollar"]
+    if c_type1 == "JPY":
+        print "exchange rate is", currencies["Japanese Yen"]
+    if c_type2 == "GBP":
+        print "to", currencies["Pound Sterling"]
+    if c_type2 == "EUR":
+        print "to", currencies["Euro"]
+    if c_type2 == "USD":
+        print "to", currencies["US Dollar"]
+    if c_type2 == "JPY":
+        print "to", currencies["Japanese Yen"]
 
+rate(c_type1, c_type2)
+
+numb1 = float(raw_input("How much %s do you wish to convert? " %c_type1))
+
+def conversion(fromCurr, toCurr, value):
+    if fromCurr == "GBP":
+        answer = value * float(currencies[short_hand[toCurr]])
+        return answer
+    elif toCurr == "GBP":
+        answer = value / float(currencies[short_hand[fromCurr]])
+    else:
+        answer = value / float(currencies[short_hand[fromCurr]])
+        answer = value * float(currencies[short_hand[toCurr]]) 
+        return answer
+
+print "%2.f %s is %2.f %s" %(numb1, short_hand[c_type1], conversion(c_type1, c_type2, numb1), short_hand[c_type2])
 #[GBP]
 
 
